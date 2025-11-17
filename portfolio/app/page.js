392729d7b92765debeb4dff66c04448c059a5289
GAURAV KADAM <gskadam3b@gmail.com>
2025-11-17 18:40:10 +0530

    Disable click spark

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 import Main from "@/components/Main";
 import Image from "next/image";
 import TargetCursor from "@/components/TargetCursor";
-import ClickSpark from "@/components/ClickSpark";
+// import ClickSpark from "@/components/ClickSpark";
 
 export default function Home() {
   return (
@@ -14,16 +14,16 @@ export default function Home() {
         hideDefaultCursor={true}
         parallaxOn={true}
       />
-      <ClickSpark
+      {/* <ClickSpark
         sparkColor='#fff'
         sparkSize={10}
         sparkRadius={15}
         sparkCount={8}
         duration={400}
-      >
+      > */}
         <Navbar />
         <Main />
-      </ClickSpark>
+      {/* </ClickSpark> */}
     </>
   );
 }
